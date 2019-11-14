f4ad6ddff833995ef3e2cc6f76635fd854567978
Ufuk Güler <ufukglr@yandex.com>
2019-11-14 23:07:50 +0300

    zimmetleme onaylama eklendi

---
+++ app/src/main/java/com/example/malzeme/ZimmetleOnayActivity.java
@@ -0,0 +1,33 @@
+package com.example.malzeme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ZimmetleOnayActivity extends AppCompatActivity {
+    ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zimmetle_onay);
+        Intent intent = getIntent();
+        list = findViewById(R.id.secilen_listview);
+        ArrayList<String> array = intent.getStringArrayListExtra("onay_array");
+        for(int i=0; i<array.size(); i++){
+            System.out.println(array.get(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, array);
+        list.setAdapter(adapter);
+    }
+
+    void onaybutonOnClick(View v) {
+
+    }
+}
