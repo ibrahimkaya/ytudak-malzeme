2a18920e9960f86518418ae5a6a0debcb6d7bc58
Ufuk Güler <ufukglr@yandex.com>
2019-11-15 13:18:02 +0300

    added json array onclick to onay button

--- app/src/main/java/com/example/malzeme/activty/ZimmetleOnayActivity.java
+++ app/src/main/java/com/example/malzeme/activty/ZimmetleOnayActivity.java
@@ -6,25 +6,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.malzeme.R;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
 public class ZimmetleOnayActivity extends AppCompatActivity {
     ListView list;
+    EditText alankisi;
+    Spinner malzemeci_spinner;
     Button btn_onay;
-
-    ArrayList<String> array = new ArrayList<>();
-    ArrayList<Integer> malzemeno = new ArrayList<>();
+    Toast hataToast;
+    JSONArray jsonArray = new JSONArray();
+    ArrayList<String> array = new ArrayList<>(); // intent'ten gelen array
+    ArrayList<Integer> malzemeno = new ArrayList<>(); // secilenlerin malzeme numaraları bu listenin icinde
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zimmetle_onay);
+        malzemeci_spinner = findViewById(R.id.malzemeci_spinner);
+        hataToast = Toast.makeText(this, "Seçim Yapmadınız!", Toast.LENGTH_SHORT);
         btn_onay = findViewById(R.id.buton_zimmetle_onay);
+        alankisi = findViewById(R.id.alankisi_edt);
         Intent intent = getIntent();
         list = findViewById(R.id.secilen_listview);
         array = intent.getStringArrayListExtra("onay_array");
@@ -32,18 +44,23 @@ public class ZimmetleOnayActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, array);
         list.setAdapter(adapter);
 
-
         btn_onay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (int x : malzemeno) {
-                    Log.e("sayilar", String.valueOf(malzemeno));
-                    //json array oluşturulcak
-                    //array içinde her bir malzeme için:
-                    //[malzemeno, zimmet alan kişinin adı, veren malzemeci]
-                    //json arrayi yapılarak gönderilecek
+                if (alankisi.getText().equals("")) {
+                    hataToast.show();
+                } else {
+                    for (int no : malzemeno) {
+                        //json array oluşturulcak array içinde her bir malzeme için: [malzemeno, zimmet alan kişinin adı, veren malzemeci] json arrayi yapılarak gönderilecek
+                        JsonObject obj = new JsonObject();
+                        obj.addProperty("mno", no);
+                        obj.addProperty("verenmalzemecino", alankisi.getText().toString());
+                        obj.addProperty("mno", malzemeci_spinner.getSelectedItem().toString());
+                        jsonArray.put(obj);
+                    }
+                    Log.e("jsonarray", jsonArray.toString());
+                    //burada sunucuya request gonderilecek
                 }
-
             }
         });
     }
@@ -55,6 +72,7 @@ public class ZimmetleOnayActivity extends AppCompatActivity {
             String parca[] = array.get(i).split(" ");
             malzemeno.add(Integer.parseInt(parca[0]));
         }
+
     }
 
 
